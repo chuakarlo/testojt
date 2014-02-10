e48779944d1ef19b2938a224b75cf61de74ed41d
mdonaldson <matthew.donaldson@schoolimprovement.com>
2014-02-10 11:51:08 -0700

    Session login using proxy to pd360 method (#2188)

--- js/models/users/SessionModel.js
+++ js/models/users/SessionModel.js
@@ -5,9 +5,38 @@ define( function ( require ) {
 	var _        = require( 'underscore' );
 	var Backbone = require( 'backbone' );
 	var Cookie   = require( 'jquery-cookie' );
-	var Vent     = require( 'Vent' );
 
 	var Session = Backbone.Model.extend( {
+
+		url : function () {
+			return '/pd360/dao/RespondService.cfc?method=rspndLogin&loginNm=' + this.username + '&passwrd=' + this.password + '&returnformat=json';
+		},
+
+		// `username` & `password` to login
+		// does not pass the username and password in the ajax call
+		'fetch' : function ( options ) {
+			var model = this;
+
+			// credentials provided
+			if ( ( options.username && options.password ) ) {
+
+				this.username = options.username;
+				this.password = options.password;
+
+				// remove username and password so that jQuery doesn't make the URL http://user:pass@domain.com
+				delete options.username;
+				delete options.password;
+
+				return this.sync( 'read', this, options );
+
+			// else no credentials provided, TODO: Error handling
+			}
+		},
+
+		// check to see if the user is logged in
+		'authenticated' : function () {
+			return Boolean( $.cookie( 'CFAUTHORIZATION_PD360' ) );
+		}
 
 	} );
 
--- js/views/users/LoginView.js
+++ js/views/users/LoginView.js
@@ -1,12 +1,60 @@
 define( function ( require ) {
 	'use strict';
 
-	var _          = require( 'underscore' );
 	var Marionette = require( 'marionette' );
+	var Backbone   = require( 'backbone' );
+	var $          = require( 'jquery' );
+	var _          = require( 'underscore' );
+	var Vent       = require( 'Vent' );
+	var Session    = require( 'Session' );
 	var template   = require( 'text!templates/users/loginView.html' );
 
 	return Marionette.ItemView.extend( {
-		'template': _.template( template )
+		'template' : _.template( template ),
+
+		'events' : {
+			'submit' : 'login'
+		},
+
+		'ui' : {
+			'username' : 'input[type=text]',
+			'password' : 'input[type=password]',
+			'loginBtn' : 'input[type=submit]'
+		},
+
+		'login' : function ( event ) {
+
+			event.preventDefault();
+
+			var username = this.ui.username.val();
+			var password = this.ui.password.val();
+
+			var valid = true;
+
+			if ( !username || !password ) {
+				valid = false;
+			}
+
+			if ( valid ) {
+
+				Session.fetch( {
+					'username' : username,
+					'password' : password,
+					'success'  : function ( jqXHR, status, error ) {
+
+						Vent.trigger( 'AppController:showSessionInfo', {} );
+
+					}.bind( this ),
+
+					'error'    : function ( jqXHR, status, error ) {
+						// TODO: error handling
+					}.bind( this )
+				} );
+
+			// else no input value, TODO: error handling
+			}
+		}
+
 	} );
 
 } );
