e50d3f57262b78cabf51bc12ec3178f802bae2a9
Joseph Panuncillo <jpanuncillo@live.com>
2014-04-11 09:25:17 +0800

    Replaced hard-coded tags with ones from the API Squashed commits: - Added Educational Level - Converted Educator Types array into map - Took data from pd360 to match returned API Ids - Added Grade Level descriptions to pd360-html

---
+++ js/apps/homepage/configuration/userDataLookup.js
@@ -0,0 +1,54 @@
+define( function ( require ) {
+	'use strict';
+
+	//from /pd360/components/videos/CCSegmentItemRenderer.mxml
+	return {
+		'gradeLevels'   : [
+			'',
+			'Pre-K',
+			'Kindergarten',
+			'1st Grade',
+			'2nd Grade',
+			'3rd Grade',
+			'4th Grade',
+			'5th Grade',
+			'6th Grade',
+			'7th Grade',
+			'8th Grade',
+			'9th Grade',
+			'10th Grade',
+			'11th Grade',
+			'12th Grade',
+			'College/University',
+			'Elementary',
+			'Middle School',
+			'Secondary',
+			'Other'
+		],
+		'educatorTypes' : {
+			''  : '',
+			'S' : 'School Administrator',
+			'I' : 'Instructional Coach',
+			'D' : 'District Administrator',
+			'O' : 'Other',
+			'T' : 'Teacher'
+		},
+		'subjects'      : [
+			'',
+			'ELA',
+			'Math',
+			'Social Studies',
+			'Foreign Language',
+			'Science',
+			'Performing Arts',
+			'Fine Arts',
+			'Music',
+			'Technology',
+			'Vocational',
+			'Consumer Science',
+			'Business',
+			'Physical Education',
+			'Other'
+		]
+	};
+} );
--- js/apps/homepage/controllers/homeController.js
+++ js/apps/homepage/controllers/homeController.js
@@ -6,43 +6,81 @@ define( function ( require ) {
 	var App      = require( 'App' );
 	var $        = require( 'jquery' );
 
-	var userTags = require( 'apps/homepage/configuration/userDummyTagData' );
+	var userData = require( 'apps/homepage/configuration/userDataLookup' );
 	var HomeView = require( 'apps/homepage/views/BaseItemView' );
 	var NullView = require( 'apps/homepage/views/BaseEmptyView' );
+
+	function hasOwnProperty ( object, key, value ) {
+		return object && object.hasOwnProperty( key ) ? object[ key ] : value;
+	}
+
+	function setUserTags ( model ) {
+		var userTags = [];
+
+		var GradeLevelId     = hasOwnProperty( model, 'GradeLevelId', 0 );
+		var EducationLevelId = hasOwnProperty( model, 'EducationLevelId', 0 );
+		var EducatorType     = hasOwnProperty( model, 'EducatorType', '' );
+		var CCSubjectId      = hasOwnProperty( model, 'CCSubjectId', 0 );
+
+		if( GradeLevelId !== 0 ) {
+			userTags.push( userData.gradeLevels[ GradeLevelId ] );
+		}
+		if( EducationLevelId !== 0 ) {
+			userTags.push( userData.gradeLevels[ EducationLevelId ] );
+		}
+		if( EducatorType !== '' ) {
+			userTags.push( userData.educatorTypes[ EducatorType ] );
+		}
+		if( CCSubjectId !== 0 ) {
+			userTags.push( userData.subjects[ CCSubjectId ] );
+		}
+
+		return userTags.join(',');
+	}
 
 	App.module( 'Homepage.Show', function ( Show ) {
 
 		// to show the user's personal info, grab the ClientPersonnel object
 		function clientProfileParams( personnelId ) {
-			return {
+			return [
+			{
 				'path'   : 'com.schoolimprovement.pd360.dao.core.ClientPersonnelGateway',
 				'method' : 'getById',
 				'args'   : {
 					'id' : personnelId
 				}
-			};
+			}, {
+				'path'   : 'com.schoolimprovement.pd360.dao.core.ClientPersonnelProfileGateway',
+				'method' : 'getById',
+				'args'   : {
+					'id' : personnelId
+				}
+			} ];
 		}
 
 		Show.Controller = {
 			'showHomepage' : function () {
 
-				if ( Session.personnelId() && Session.personnelId() in userTags) {
+				if ( Session.personnelId() ) {
 
-					var fetchingModels = Remoting.fetch( [ clientProfileParams( Session.personnelId() ) ] );
+					var fetchingModels = Remoting.fetch( clientProfileParams( Session.personnelId() ) );
 
 					$.when( fetchingModels ).done( function ( models ) {
+
 						App.reqres.setHandler( 'homepage:userProfile', function () {
 							return models[0];
 						} );
 
 						App.reqres.setHandler( 'homepage:userTags', function () {
-							return userTags[Session.personnelId()];
+							return setUserTags( models[1] );
 						} );
 
 						App.content.show( new HomeView() );
 
 					} ).fail( function ( error ) {
+
 						App.content.show( new NullView() );
+
 					} );
 
 				} else {
