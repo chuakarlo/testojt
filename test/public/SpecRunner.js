c9bef7087b74d771caa7f98247712fb693751ab2
Michael Tuttle <openam@gmail.com>
2014-04-30 10:27:23 -0600

    Remove on DOM ready from SpecRunner

--- test/public/SpecRunner.js
+++ test/public/SpecRunner.js
@@ -11,29 +11,23 @@
 		require( 'jquery-cookie' );
 		require( 'jquery-placeholder' );
 
-		var $            = require( 'jquery' );
 		var sinon        = require( 'sinon-chai' );
 		var suite        = require( 'spec/suite' );
 		var chaiBackbone = require( 'chai-backbone' );
 		var chaiChanges  = require( 'chai-changes' );
 
-		// on dom ready require all specs and run
-		$( function () {
+		require( suite.specs, function () {
 
-			require( suite.specs, function () {
+			window.chai.should();
+			window.chai.use( sinon );
+			window.chai.use( chaiBackbone );
+			window.chai.use( chaiChanges );
 
-				window.chai.should();
-				window.chai.use( sinon );
-				window.chai.use( chaiBackbone );
-				window.chai.use( chaiChanges );
-
-				if ( window.mochaPhantomJS ) {
-					window.mochaPhantomJS.run();
-				} else {
-					window.mocha.run();
-				}
-
-			} );
+			if ( window.mochaPhantomJS ) {
+				window.mochaPhantomJS.run();
+			} else {
+				window.mocha.run();
+			}
 
 		} );
 
