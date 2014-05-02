91a69e8f46d7dd6c71e7e92dbdec9869360901a9
Rey Philip Regis <rey.regis@globalzeal.net>
2014-05-02 11:25:37 +0800

    Fixed alignments CatalogsItemView.js

--- js/apps/learningTargets/views/catalogs/CatalogsItemView.js
+++ js/apps/learningTargets/views/catalogs/CatalogsItemView.js
@@ -1,15 +1,15 @@
 define( function ( require ) {
 	'use strict';
 
-	var Marionette      = require( 'marionette' );
-	var template        = require( 'text!apps/learningTargets/templates/catalogs/catalog.html' );
-	var _               = require( 'underscore' );
+	var Marionette = require( 'marionette' );
+	var template   = require( 'text!apps/learningTargets/templates/catalogs/catalog.html' );
+	var _          = require( 'underscore' );
 
 	return Marionette.ItemView.extend( {
 		'template' : _.template( template ),
 		'tagName'  : 'li',
 		'ui'       : {
-			'drawerToggleButton'  : '.lt-toggle-btn'
+			'drawerToggleButton' : '.lt-toggle-btn'
 		},
 		'events' : {
 			'click div.catalog-item' : 'showCatalogDescription'
@@ -20,5 +20,4 @@ define( function ( require ) {
 			this.trigger( 'lt:showdescription' );
 		}
 	} );
-
 } );
