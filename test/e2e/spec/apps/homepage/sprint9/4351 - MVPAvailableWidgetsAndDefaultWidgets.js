f3f7605fa1ef847c489fb24321d4077dfe0d78e2
saudia tepace <saudiatepace@yahoo.com>
2014-05-13 11:52:38 +0800

    Revised Selenium Test (4351)

---
+++ test/e2e/spec/apps/homepage/sprint9/4351 - MVPAvailableWidgetsAndDefaultWidgets.js
@@ -0,0 +1,109 @@
+'use strict';
+
+var suite = require( 'selenium-runner' ).suite;
+
+suite( function( env ) {
+
+	describe( 'MVP Available Widgets and Default Widgets', function() {
+
+		var browser;
+
+
+		var CSS = {
+			'yourProfile' : 'yourProfile',
+			'focusObjective' : 'focusObjective',
+			'observationsOfMe' : 'observationsOfMe'
+		};
+
+		
+		describe( 'Default Widgets Display', function () {
+
+			before( function() {
+
+				browser = env.browser;
+
+			} );
+
+			it( 'should log you in', function( done ) {
+
+				browser
+					.elementById( 'login-input-email' ).clear().type( 'david.rojas@schoolimprovement.com' )
+					.elementById( 'login-input-password' ).clear().type( 'pd360' )
+					.elementById( 'login-button' ).click()
+					.waitForElement( 'id', 'Home-page-view' )
+					.nodeify( done );
+			} );
+
+
+			it( 'should display the default widgets: Observations of me, User Settings and Focus Objective', function ( done ) {
+
+				browser.maximize();
+
+				browser
+					.elementByCssSelector( '#active-widgets #' + CSS.observationsOfMe )
+					.elementByCssSelector( '#active-widgets #' + CSS.yourProfile )
+					.elementByCssSelector( '#active-widgets #' + CSS.focusObjective )
+					.takeScreenshot().saveScreenshot('../sc/default/' + new Date() )
+					.nodeify( done );
+			} );
+
+		} );
+
+		describe( 'MVP Available Widgets', function () {
+
+			it( 'should display configuration settings overlay', function ( done ) {
+				browser
+
+					.elementById( 'widget-settings' ).click()
+					.elementByClassName( 'opened' )
+					.nodeify( done );
+			});
+
+			it( 'should be able to add/remove all available widgets in configuration settings', function ( done ) {
+				browser
+
+					.elementById( 'widget-settings-selection' )
+					.elementByCssSelector( '#widget-settings-selection > li:nth-child(1)' ).text().then( function ( text ) {
+						text.should.equal( 'Courses' );
+					} )
+					.elementByCssSelector( '#widget-settings-selection > li:nth-child(2)' ).text().then( function ( text ) {
+						text.should.equal( '3\nFocus Objectives' );
+					} )
+					.elementByCssSelector( '#widget-settings-selection > li:nth-child(3)' ).text().then( function ( text ) {
+						text.should.equal( 'Group Activity' );
+					} )
+					.elementByCssSelector( '#widget-settings-selection > li:nth-child(4)' ).text().then( function ( text ) {
+						text.should.equal( '2\nUser Settings' );
+					} )
+					.elementByCssSelector( '#widget-settings-selection > li:nth-child(5)' ).text().then( function ( text ) {
+						text.should.equal( '1\nObservations Of Me' );
+					} )
+
+					//remove default widget in What To Do Next section
+					.elementByCssSelector( '#widget-settings-selection > li:nth-child(2) > div.active.widget-icon-btn.img-circle.fa.fa-minus' ).click()
+
+					//add another widget in What To Do Next section
+					.elementByCssSelector( '#widget-settings-selection > li:nth-child(1) > div.inactive.widget-icon-btn.img-circle.fa.fa-plus' ).click()
+
+					//display back the three default widgets
+					.elementByCssSelector( '#widget-settings-selection > li:nth-child(1) > div.active.widget-icon-btn.img-circle.fa.fa-minus' ).click()
+
+					.elementByCssSelector( '#widget-settings-selection > li:nth-child(2) > div.inactive.widget-icon-btn.img-circle.fa-plus.fa' ).click()
+					
+					
+					.elementById( 'widget-settings' ).click()
+					.nodeify( done );
+			} );
+
+			it( 'should be working in Chrome, Firefox and Safari', function ( done ) {
+				browser
+					.hasElement( 'id' , 'active-widgets' )
+					.nodeify( done );
+			} );
+
+		} );
+	} );
+
+} );
+
+
