8612afa5d8756f9ce0553acb5a7181f7c823da23
Miriam Mae Iglesia <miriamiglesia@gmail.com>
2014-05-19 16:43:27 +0800

    Added back base.js of null widget(4884)

---
+++ js/apps/homepage/external/widgets/external/null/base.js
@@ -0,0 +1,31 @@
+define( function ( require ) {
+	'use strict';
+
+	var BaseObj      = require( 'apps/homepage/BaseObject' );
+	var instance     = new BaseObj();
+
+	instance._id = 'widgets';
+
+	return instance.extend( {
+		'WidgetId'    : 0,
+		'WidgetName'  : function () {
+			return 'I am empty';
+		},
+		'header'      : function () {
+			return 'I am empty';
+		},
+		'footer'      : function () {
+			return 'Add more widgets';
+		},
+		'Description' : function () {
+			return '';
+		},
+		'imgSrc'      : function () {
+			return '';
+		},
+		'icon'        : function () {
+			return '';
+		},
+		'em'          : 8.5
+	} );
+} );
