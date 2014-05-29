a89b713ab1c440b8429e7a0f224c28e778764d6d
Alexjander Bacalso <janderbacalso@gmail.com>
2014-05-29 13:32:36 +0800

    Fixed inconsistent process status and task status

--- js/apps/learningTargets/models/ProcessesModel.js
+++ js/apps/learningTargets/models/ProcessesModel.js
@@ -7,42 +7,52 @@ define( function ( require ) {
 	return Backbone.Model.extend( {
 
 		'parse' : function ( model ) {
-
-			this.setProcessStatusColor ( model );
+			this.setProcessStatus ( model );
 			this.setTaskStatus ( model );
-
 			return model;
 		},
 
 		'_convertToTimeStamp' : function ( date ) {
-			var newDate = new Date( date );
-			return ( newDate.getTime() / 100 );
+			return ( new Date( date ).getTime() / 100 );
+		},
+
+		'_getStatus' : function ( duedate, currentdate, initial ) {
+			var status = {
+				message : initial || false,
+				color   : 'danger'
+			};
+			if ( duedate >= currentdate) {
+				status.message = 'Current' || true;
+				status.color   = 'success';
+			}
+			return status;
 		},
 
 		'setTaskStatus' : function ( model ) {
 			var self = this;
 
+			var currentDate       = this._convertToTimeStamp( new Date() );
 			var mainCompletedDate = this._convertToTimeStamp( model.CompleteByDate );
+			var ProcessStatus     = this._getStatus( mainCompletedDate, currentDate, '' );
 
 			_.map( model.Tasks, function ( tasksObj ) {
 				var taskCompletedDate = self._convertToTimeStamp( tasksObj.CompleteByDate );
 
-				tasksObj.status      = 'Not Current';
-				tasksObj.statusColor = 'step-not-current';
-
-				if ( taskCompletedDate <= mainCompletedDate ) {
-					tasksObj.status      = 'Current ';
-					tasksObj.statusColor = 'step-current';
+				tasksObj.StepStatus = self._getStatus( taskCompletedDate, mainCompletedDate, 'Not Current' );
+				if ( !ProcessStatus.message ) {
+					tasksObj.StepStatus  = self._getStatus( taskCompletedDate, currentDate, 'Past Due' );
 				}
 			} );
 
 			return model;
 		},
 
-		'setProcessStatusColor' : function ( model ) {
-			model.statusColor = 'step-not-current';
-			if ( model.ProcessStatus === 'Current' || model.ProcessStatus === '' ) {
-				model.statusColor  = 'step-current';
+		'setProcessStatus' : function ( model ) {
+			var currentDate       = this._convertToTimeStamp( new Date() );
+			var mainCompletedDate = this._convertToTimeStamp( model.CompleteByDate );
+
+			if ( !model.ProcessStatus.length ) {
+				model.ProcessStatus = this._getStatus ( mainCompletedDate, currentDate, 'Not Current');
 			}
 
 			return model;
--- js/apps/learningTargets/views/processes/TasksItemView.js
+++ js/apps/learningTargets/views/processes/TasksItemView.js
@@ -6,40 +6,7 @@ define( function ( require ) {
 	var _          = require( 'underscore' );
 
 	return Marionette.ItemView.extend( {
-		'template'   : _.template( template ),
-
-		'_convertToTimeStamp' : function ( date ) {
-			var newDate = new Date( date );
-			return ( newDate.getTime() / 100 );
-		},
-
-		'setTaskStatus' : function ( model ) {
-			var self = this;
-
-			var mainCompletedDate = this._convertToTimeStamp( model.get( 'CompleteByDate' ) );
-
-			_.map( model.get( 'Tasks' ), function ( tasksObj ) {
-				var taskCompletedDate = self._convertToTimeStamp( tasksObj.CompleteByDate );
-
-				tasksObj.status      = 'Not Current';
-				tasksObj.statusColor = 'step-not-current';
-
-				if ( taskCompletedDate <= mainCompletedDate ) {
-					tasksObj.status      = 'Current ';
-					tasksObj.statusColor = 'step-current';
-				}
-			} );
-
-			return model;
-		},
-
-		templateHelpers : function () {
-			var self = this;
-
-			self.setTaskStatus ( self.model );
-
-			return self.model;
-		}
+		'template' : _.template( template )
 	} );
 
 } );
