3cccbe3ab3d50face8436fa95727d0c9d1029b77
shinji escorido <shinji.escorido@gmail.com>
2014-06-02 14:05:51 +0800

    Fixed process widget mobility issues

--- js/apps/homepage/external/widgets/external/processOfMe/views/WidgetItemView.js
+++ js/apps/homepage/external/widgets/external/processOfMe/views/WidgetItemView.js
@@ -1,22 +1,24 @@
 define( function ( require ) {
 	'use strict';
 
-	var App        = require( 'App' );
-	var Marionette = require( 'marionette' );
-	var _          = require( 'underscore' );
-	var moment     = require( 'moment' );
-	var template   = require( 'text!apps/homepage/external/widgets/external/processOfMe/templates/widgetItemView.html' );
-	var getConfig  = require( 'common/helpers/getConfig' );
+	var App          = require( 'App' );
+	var Marionette   = require( 'marionette' );
+	var _            = require( 'underscore' );
+	var moment       = require( 'moment' );
+	var template     = require( 'text!apps/homepage/external/widgets/external/processOfMe/templates/widgetItemView.html' );
+	var getConfig    = require( 'common/helpers/getConfig' );
+	var className    = 'col-md-12 no-padding widget-item';
+	var templateBind = _.template( template );
 
 	return Marionette.ItemView.extend( {
-		'template'        : _.template( template ),
-		'className'       : 'col-md-12 no-padding widget-item',
+		'template'        : templateBind,
+		'className'       : className,
 		'templateHelpers' : function () {
 			return {
-				'due'       : this.getDueDate(),
-				'ContentId' : '#resources/learning/processes',
-				'content'   : App.Homepage.Utils.limitCharacters( this.model.get( 'ProcessName' ), 37 ),
-				'getConfig' : getConfig( 'contentThumbnailPath' )
+				'due'         : this.getDueDate(),
+				'ContentId'   : '#resources/learning/processes',
+				'creatorName' : App.Homepage.Utils.limitCharacters( this.model.get( 'ProcessName' ), 35 ),
+				'getConfig'   : getConfig( 'contentThumbnailPath' )
 			};
 		},
 		'getDueDate'      : function () {
