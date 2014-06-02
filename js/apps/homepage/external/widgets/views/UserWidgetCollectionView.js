0c9c25404348a32beb851865e9b460db8d27d2d1
Miriam Mae Iglesia <miriamiglesia@gmail.com>
2014-06-02 17:44:10 +0800

    Fixed active widget sizes in diff resolutions  (4984)

--- js/apps/homepage/external/widgets/views/UserWidgetCollectionView.js
+++ js/apps/homepage/external/widgets/views/UserWidgetCollectionView.js
@@ -1,7 +1,9 @@
 define( function ( require ) {
 	'use strict';
 
-	var Marionette = require( 'marionette' );
+	var Marionette      = require( 'marionette' );
+	var $               = require( 'jquery' );
+	var MAX_WIDGET_PEEK = 30;
 
 	var UserWidgetItemView      = require( 'apps/homepage/external/widgets/views/UserWidgetCompositeView' );
 	var EmptyUserWidgetItemView = require( 'apps/homepage/external/widgets/views/EmptyUserWidgetItemView' );
@@ -19,11 +21,40 @@ define( function ( require ) {
 		} );
 	}
 
+/**
+ * Adjust individual widget margin on different screen resolution if last Widget PEEK
+ * percentage is less than 30%
+ */
+
+	function adjustWidgetMargin ( self ) {
+
+		var nWidgetWid = $( '#active-widgets' ).width();
+		var nItemWid   = self.$el.find( 'li.widget-specific:first' ).outerWidth();
+		var nRemainder = nWidgetWid % nItemWid;
+		var nPeek      = ( nRemainder > 0 ) ? (  ( nRemainder / nItemWid ) * 100 ) : 0;
+
+		if ( nPeek < MAX_WIDGET_PEEK ) {
+			var nActive    = Math.floor( nWidgetWid / nItemWid );
+			self.$el.find( 'li.widget-specific' ).css( { 'margin' : '0 ' + ( nRemainder / ( nActive * 2 ) ) + 'px' } );
+		}
+	}
+
 	return Marionette.CollectionView.extend( {
+
+		'initialize' : function () {
+			$( window ).resize( function () {
+				adjustWidgetMargin( this );
+			}.bind( this ) );
+		},
+
 		'tagName'   : 'ul',
 		'itemView'  : UserWidgetItemView,
 		'className' : 'active-widgets-container no-padding',
+
 		'onShow'    : function () {
+
+			adjustWidgetMargin( this );
+
 			var self  = this;
 			var count = 3 - this.collection.length;
 
