d35b8baf92525aee856d775582ef6c7c818967e9
Cyril Basong <sairexes@gmail.com>
2014-06-04 07:55:29 +0800

    Added redirect to help for widget with no data link. (4891)

--- js/apps/homepage/external/widgets/views/EmptyWidgetView.js
+++ js/apps/homepage/external/widgets/views/EmptyWidgetView.js
@@ -1,14 +1,21 @@
 define( function ( require ) {
 	'use strict';
 
-	var Marionette      = require( 'marionette' );
-	var _               = require( 'underscore' );
-	var template        = require( 'text!apps/homepage/external/widgets/templates/EmptyWidgetView.html' );
+	var Marionette = require( 'marionette' );
+	var _          = require( 'underscore' );
+	var App        = require( 'App' );
+	var template   = require( 'text!apps/homepage/external/widgets/templates/EmptyWidgetView.html' );
 
 	var className = 'col-md-12 no-padding empty-widget-holder';
 
 	return Marionette.ItemView.extend( {
 		'template'        : _.template( template ),
+		'ui'              : {
+			'helpLink' : '#emptywidgetHelp'
+		},
+		'events'          : {
+			'click @ui.helpLink' : 'redirectToHelp'
+		},
 		'className'       : function () {
 			return className;
 		},
@@ -17,6 +24,19 @@ define( function ( require ) {
 				'content' : this.EmptyMessage,
 				'icon'    : this.EmptyType
 			};
+		},
+		'redirectToHelp'  : function () {
+
+			var personnel   = App.request( 'homepage:userProfile' );
+			var email       = 'email='       + personnel.EmailAddress;
+			var fname       = 'fname='       + personnel.FirstName;
+			var lname       = 'lname='       + personnel.LastName;
+			var personnelid = 'personnelid=' + personnel.PersonnelId;
+			var location    = 'url=' + window.location + '#widgets';
+			var url         = 'http://help.schoolimprovement.com/#context/' + [ email, fname, lname, personnelid, location ].join( '&' );
+
+			this.ui.helpLink.attr( 'href', url );
 		}
+
 	} );
 } );
