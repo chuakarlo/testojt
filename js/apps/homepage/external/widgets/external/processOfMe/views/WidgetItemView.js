c5311b2ca02703a9a42abbbae26c01f84314fd1a
shinji escorido <shinji.escorido@gmail.com>
2014-06-05 13:47:28 +0800

    Fixed icon and date status

--- js/apps/homepage/external/widgets/external/processOfMe/views/WidgetItemView.js
+++ js/apps/homepage/external/widgets/external/processOfMe/views/WidgetItemView.js
@@ -11,19 +11,48 @@ define( function ( require ) {
 	var templateBind = _.template( template );
 
 	return Marionette.ItemView.extend( {
-		'template'        : templateBind,
-		'className'       : className,
-		'templateHelpers' : function () {
+		'template'         : templateBind,
+		'className'        : function () {
+			return className + this.doCheckStatus();
+		},
+		'templateHelpers'  : function () {
 			return {
 				'due'         : this.getDueDate(),
+				'dateStatus'  : this.getDateStatus(),
+				'textStatus'  : this.getTextDueStatus(),
 				'ContentId'   : '#resources/learning/processes',
 				'creatorName' : App.Homepage.Utils.limitCharacters( this.model.get( 'ProcessName' ), 35 ),
 				'getConfig'   : getConfig( 'contentThumbnailPath' )
 			};
 		},
-		'getDueDate'      : function () {
+		'getDueDate'       : function () {
 			var now = moment( this.model.get( 'CompleteByDate' ) ).format( 'M-D-YYYY' );
 			return now;
+		},
+		'doCheckStatus'    : function () {
+			var completeByDate = this.model.get( 'CompleteByDate' );
+			if ( moment().isAfter( completeByDate ) ) {
+				return ' olderdate';
+			} else {
+				return '';
+			}
+		},
+		'getDateStatus'    : function () {
+			var completeByDate = this.model.get( 'CompleteByDate' );
+			if ( moment().isAfter( completeByDate ) ) {
+				return ' olddate';
+			} else {
+				return '';
+			}
+		},
+		'getTextDueStatus' : function () {
+			var completeByDate = this.model.get( 'CompleteByDate' );
+			if ( moment().isAfter( completeByDate ) ) {
+				return 'Past Due : ';
+			} else {
+				return 'Due : ';
+			}
 		}
+
 	} );
 } );
