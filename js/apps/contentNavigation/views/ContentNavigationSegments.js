b52221d51617817667256cd44986a306c179ce1e
Vincent John Cagay <vjcagay@gmail.com>
2014-06-05 11:24:34 +0800

    Fixed Segment Card resizing behavior in Content Navigation
    - Thumbnail image responsive behaviour changed from stretching to zooming in/out
    - Adjusted segment card width to cope with Thumbnail image responsive behavior change

--- js/apps/contentNavigation/views/ContentNavigationSegments.js
+++ js/apps/contentNavigation/views/ContentNavigationSegments.js
@@ -23,7 +23,7 @@ define( function ( require ) {
 			};
 
 			var viewOptions = {
-				'className' : 'col-xs-12 col-sm-6 col-md-4 col-lg-4'
+				'className' : 'col-xs-12 col-sm-12 col-md-6 col-lg-4'
 			};
 
 			var options = ( !model.get( 'UUVideoId' ) && !model.get( 'ContentId' ) ) ? emptyViewOptions : viewOptions;
