c4592fb5abc5b8c33fa19d798c8a7c7f12501133
Mary Jesany Anub <mjanub@gmail.com>
2014-06-17 12:57:12 +0800

    Learning Targets - Modal UI Enhancements (5512, 5573)

--- js/apps/learningTargets/views/catalogs/DescriptionView.js
+++ js/apps/learningTargets/views/catalogs/DescriptionView.js
@@ -4,7 +4,7 @@ define( function ( require ) {
 	var Marionette = require( 'marionette' );
 	var template   = require( 'text!apps/learningTargets/templates/catalogs/description.html' );
 	var _          = require( 'underscore' );
-	var getTime    = require( 'common/helpers/getTime' );
+	var moment     = require( 'moment' );
 
 	return Marionette.ItemView.extend( {
 		'template' : _.template( template ),
@@ -12,11 +12,15 @@ define( function ( require ) {
 		'templateHelpers' : function () {
 			return {
 				getStartTime : function () {
-					return getTime( this.StartTime, true );
+					return moment( this.StartTime ).format( 'dddd MMMM DD, YYYY' );
 				},
-
-				getEndTime : function () {
-					return getTime( this.EndTime, true );
+				getEndTime   : function () {
+					return moment( this.EndTime ).format( 'dddd MMMM DD, YYYY' );
+				},
+				getTimeDiff  : function () {
+					var start = moment( this.StartTime ).format( 'HH:mm A' );
+					var end = moment( this.EndTime ).format( 'HH:mm A' );
+					return start + ' - ' + end;
 				}
 			};
 		}
