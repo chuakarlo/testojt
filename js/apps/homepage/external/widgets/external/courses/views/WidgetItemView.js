c2e8585875b54807163fbb2a9c5a7fc73e62ba23
Cyril Basong <sairexes@gmail.com>
2014-06-18 14:21:20 +0800

    Refactor course redirect link to legacy. (5410)

--- js/apps/homepage/external/widgets/external/courses/views/WidgetItemView.js
+++ js/apps/homepage/external/widgets/external/courses/views/WidgetItemView.js
@@ -2,12 +2,11 @@ define( function ( require ) {
 	'use strict';
 
 	var App              = require( 'App' );
-	var $                = require( 'jquery' );
 	var Marionette       = require( 'marionette' );
 	var _                = require( 'underscore' );
 	var progressTemplate = require( 'text!apps/homepage/external/widgets/external/courses/templates/progressItemTemplate.html' );
 
-	var widgetDirectory = 'resources/learning/courses';
+	var widgetDirectory = 'resources/learning/courses/';
 
 	function setTemplateHelpers ( model ) {
 		return {
@@ -37,20 +36,8 @@ define( function ( require ) {
 			doOnShow( this, this.model );
 		},
 		'redirect'        : function ( e ) {
-			e.preventDefault();
-			var self = this;
-			var pd360Loaded = App.request( 'pd360:loaded' );
-			var LoadingView = new App.Common.LoadingView() ;
-			var CourseID = self.model.get( 'COURSEID' );
-
-			App.content.show( LoadingView );
-			App.navigate( widgetDirectory + '/' + CourseID + '/legacy');
-
-			App.when( pd360Loaded ).done( function () {
-				LoadingView.close();
-				$( '.content-wrapper' ).html( '' );
-				App.request( 'pd360:navigate', 'courses', 'coursesBrowse', {  'COURSEID' : CourseID } );
-			} );
+			App.Homepage.Utils.redirect( e, widgetDirectory );
+			return false;
 		},
 
 		'onRender'        : function ( ) {
