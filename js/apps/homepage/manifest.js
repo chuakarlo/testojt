25bafe2c641701bf50605dc37925cb69f1000eb6
Cyril Basong <sairexes@gmail.com>
2014-06-19 17:51:06 +0800

    change manifest for content.

--- js/apps/homepage/manifest.js
+++ js/apps/homepage/manifest.js
@@ -1,16 +1,17 @@
 define( function ( require ) {
 	'use strict';
 
-	return function () {
+	return function ( ) {
 
-		var parent     = [];
-		var sharedData = {};
+		var parent     = [ ];
+		var sharedData = { };
 
 		//Register your external sections here
 		require( 'apps/homepage/external/billboard/base' ).register( parent, sharedData );
 		require( 'apps/homepage/external/widgets/base' ).register( parent, sharedData );
-		require( 'apps/homepage/external/content2/base' ).register( parent, sharedData );
+		require( 'apps/homepage/external/content/base' ).register( parent, sharedData );
+		// require( 'apps/homepage/external/content2/base' ).register( parent, sharedData );
 
 		return parent;
 	};
-});
+} );
