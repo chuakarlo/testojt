0aeaf78da3aa6b2457cc2de791d6ca562f5edfbe
Joseph Panuncillo <jpanuncillo@live.com>
2014-06-19 10:52:19 +0800

    Fixed remaining process widget issues

--- js/apps/homepage/external/widgets/external/processOfMe/views/WidgetItemView.js
+++ js/apps/homepage/external/widgets/external/processOfMe/views/WidgetItemView.js
@@ -2,14 +2,14 @@ define( function ( require ) {
 	'use strict';
 
 	var App           = require( 'App' );
-	var $             = require( 'jquery' );
 	var Marionette    = require( 'marionette' );
 	var _             = require( 'underscore' );
 	var moment        = require( 'moment' );
 	var template      = require( 'text!apps/homepage/external/widgets/external/processOfMe/templates/widgetItemView.html' );
-	var ProcessesView = require( 'apps/learningTargets/views/processes/ProcessesView' );
 	var getConfig     = require( 'common/helpers/getConfig' );
 	var className     = 'col-md-12 no-padding widget-item';
+
+	var widgetDirectory = 'resources/learning/observations/';
 	var templateBind  = _.template( template );
 
 	var message = App.Homepage.Utils.message;
@@ -27,20 +27,7 @@ define( function ( require ) {
 		},
 		'showProcessStep'  : function ( e ) {
 			e.preventDefault();
-
-			var self = this;
-			var pd360Loaded = App.request( 'pd360:loaded' );
-
-			App.navigate( '#resources/learning/processes/legacy' );
-
-			App.content.show( new App.Common.LoadingView() );
-
-			App.when( pd360Loaded ).done( function () {
-				App.content.show( new ProcessesView() );
-				$( '.content-wrapper' ).html( '' );
-				App.request( 'pd360:navigate', 'observation', 'observationProcessesOfMe', { 'processId' : self.model.get( 'ProcessId' ), 'processTaskId' : self.model.get( 'Tasks' )[0].ProcessTaskId } );
-
-			} );
+			App.Homepage.Utils.redirect( e, widgetDirectory );
 		},
 		'templateHelpers'  : function () {
 			return {
@@ -55,6 +42,7 @@ define( function ( require ) {
 		},
 		'getDueDate'       : function () {
 			var now = moment( this.model.get( 'CompleteByDate' ) ).format( 'M-D-YYYY' );
+			now = now === 'Invalid date' ? '' : now;
 			return now;
 		},
 		'doCheckStatus'    : function () {
