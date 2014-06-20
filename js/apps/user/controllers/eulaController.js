abcce82e5e8f1752a5e5ab86da2b639a68894cd8
Michael Tuttle <openam@gmail.com>
2014-06-20 10:54:46 -0600

    Updated EULA for possible intermittent failure on submit (5665)

--- js/apps/user/controllers/eulaController.js
+++ js/apps/user/controllers/eulaController.js
@@ -2,36 +2,37 @@ define( function ( require ) {
 	'use strict';
 
 	var App         = require( 'App' );
-	var EulaLayout  = require( 'user/views/eula/EulaLayout' );
 	var EulaContent = require( 'user/views/eula/EulaContent' );
+	var EulaLayout  = require( 'user/views/eula/EulaLayout' );
 
 	App.module( 'User.Eula', function ( Eula ) {
 
 		Eula.Controller = {
 
 			'showEula' : function () {
-				var personnelRequest = App.request( 'user:personnel' );
 
-				// show a loading view while data is fetching
-				App.content.show( new App.Common.LoadingView() );
+				// If EULA page is refreshed the session won't exist quite yet
+				// The catchall handler will retry when personnel exists
+				if ( App.request( 'session:personnel' ) ) {
 
-				App.when( personnelRequest ).done( function ( personnel ) {
+					var personnel = new App.Entities.Personnel( App.request( 'session:personnel' ) );
 
-					var eulaContent = new EulaContent();
 					var eulaLayout  = new EulaLayout( {
 						'model' : personnel
 					} );
 
+					// Show EULA layout
 					App.content.show( eulaLayout );
 
-					eulaLayout.eulaRegion.show( eulaContent );
+					// Add the EULA Content to the layout
+					eulaLayout.eulaRegion.show( new EulaContent() );
 
 					App.vent.trigger( 'flash:message', {
 						'message' : 'Before proceeding to PD 360, you must accept the terms of the End User License Agreement.',
 						'type'    : 'error'
 					} );
 
-				} ).fail( App.errorHandler.bind( App, { 'region' : App.content } ) );
+				}
 
 			}
 
