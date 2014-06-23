e041cb149e26d24a566a067ccf1bbaa21f1f45bf
shinji escorido <shinji.escorido@gmail.com>
2014-06-23 17:38:33 +0800

    Added Process of me widget Schema validation 5768

--- js/apps/homepage/external/widgets/external/processOfMe/collections/WidgetCollection.js
+++ js/apps/homepage/external/widgets/external/processOfMe/collections/WidgetCollection.js
@@ -1,10 +1,27 @@
 define ( function (require ) {
 	'use strict';
 
-	var Backbone = require( 'backbone' );
-	var Remoting = require( 'Remoting' );
-	var Session  = require( 'Session' );
-	var App      = require( 'App' );
+	var Backbone         = require( 'backbone' );
+	var Remoting         = require( 'Remoting' );
+	var Session          = require( 'Session' );
+	var App              = require( 'App' );
+	var ValidationSchema = {
+		'title'      : 'process of me schema',
+		'type'       : 'object',
+		'required'   : [ 'ProcessName', 'CompleteByDate', 'ProcessId' ],
+		'properties' : {
+			'ProcessName'    : {
+				'type' : 'string'
+			},
+			'CompleteByDate' : {
+				'type' : 'string'
+			},
+			'ProcessId'      : {
+				'type'        : 'number',
+				'uniqueItems' : true
+			}
+		}
+	};
 
 	function widgetRequest ( personnelId ) {
 		return {
@@ -23,6 +40,16 @@ define ( function (require ) {
 
 			App.when( fetchingModels ).done( function ( models ) {
 				options.success( new Backbone.Collection( models[ 0 ] ) );
+				App.Homepage.Utils.jsonVal( ValidationSchema, models[0].slice(0,5), function ( err ) {
+					if ( !err ) {
+						//App.vent.trigger( 'flash:message', {
+						options.success( new Backbone.Collection( models[0].slice(0,5) ) );
+						return;
+					}
+					App.vent.trigger( 'flash:message', {
+						'message' : 'Process of me widget: JSon error'
+					} );
+				} );
 			} ).fail( function ( error ) {
 				App.vent.trigger( 'flash:message', {
 					'message' : App.Homepage.Utils.message.processOfMeErrMsg
