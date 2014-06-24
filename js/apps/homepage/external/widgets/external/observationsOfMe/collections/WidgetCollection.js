8d582b4fd8efd70e36498c26044d745a8e1bf19b
shinji escorido <shinji.escorido@gmail.com>
2014-06-24 08:32:21 +0800

    Schema verification for Observations of me widget 5767

--- js/apps/homepage/external/widgets/external/observationsOfMe/collections/WidgetCollection.js
+++ js/apps/homepage/external/widgets/external/observationsOfMe/collections/WidgetCollection.js
@@ -1,12 +1,31 @@
 define ( function ( require ) {
 	'use strict';
 
-	var Backbone    = require( 'backbone' );
-	var Remoting    = require( 'Remoting' );
-	var $           = require( 'jquery' );
-	var Session     = require( 'Session' );
-	var App         = require( 'App' );
-
+	var Backbone         = require( 'backbone' );
+	var Remoting         = require( 'Remoting' );
+	var $                = require( 'jquery' );
+	var Session          = require( 'Session' );
+	var App              = require( 'App' );
+	var ValidationSchema = {
+		'title'      : 'observations of me schema',
+		'type'       : 'object',
+		'required'   : [ 'OBSERVATIONDATE', 'NUMBEROFPRESCRIBEDPD', 'OBSERVATIONNAME', 'OBSERVATIONID' ],
+		'properties' : {
+			'OBSERVATIONDATE'      : {
+				'type' : 'string'
+			},
+			'NUMBEROFPRESCRIBEDPD' : {
+				'type' : 'number'
+			},
+			'OBSERVATIONNAME'      : {
+				'type' : 'string'
+			},
+			'OBSERVATIONID'        : {
+				'type'        : 'number',
+				'uniqueItems' : true
+			}
+		}
+	};
 	function widgetRequest ( personnelId ) {
 		return {
 			'path'   : 'com.schoolimprovement.pd360.dao.ObservationService',
@@ -29,8 +48,17 @@ define ( function ( require ) {
 			var fetchingModels = Remoting.fetch( [ widgetRequest( Session.personnelId() ) ] );
 
 			$.when( fetchingModels ).done( function ( models ) {
-
 				options.success( new Collection( models[ 0 ] ) );
+				App.Homepage.Utils.jsonVal( ValidationSchema, models[ 0 ], function ( err ) {
+					if ( !err ) {
+						options.success( new Backbone.Collection( models[ 0 ] ) );
+						return;
+					} else {
+						App.vent.trigger( 'flash:message', {
+							'message' : 'Obervations of me widget: JSon error'
+						} );
+					}
+				} );
 
 			} ).fail( function ( error ) {
 
