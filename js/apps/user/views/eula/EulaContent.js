78f5849c262563a6fab2d6ea350c85df42f9a1cb
Michael Tuttle <openam@gmail.com>
2014-06-30 08:59:56 -0600

    Updated EULA to show full link to privacy policy

--- js/apps/user/views/eula/EulaContent.js
+++ js/apps/user/views/eula/EulaContent.js
@@ -7,7 +7,23 @@ define( function ( require ) {
 
 	return Marionette.ItemView.extend( {
 
-		'template' : _.template( template )
+		'template' : _.template( template ),
+
+		'ui' : {
+			'privacyLinks' : '.js-privacy'
+		},
+
+		'onRender' : function () {
+			var url = window.location.protocol + '//' + window.location.hostname;
+
+			if ( window.location.port ) {
+				url += ':' + window.location.port;
+			}
+
+			url	+= window.location.pathname + '#privacy';
+
+			this.ui.privacyLinks.html( url );
+		}
 
 	} );
 
