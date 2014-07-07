46f8ccddd2a16a1a6081a98c449074705bb76fda
Justin Morris <morris.justin@gmail.com>
2014-07-07 17:15:59 -0600

    Updated link for closed caption files (6234)

--- js/apps/videoPlayer/views/player/VideoPlayerView.js
+++ js/apps/videoPlayer/views/player/VideoPlayerView.js
@@ -51,7 +51,7 @@ define( function ( require ) {
 			},
 
 			'getCC' : function () {
-				var url         = 'PD360/media/video/';
+				var url         = 'subtitles/';
 				var folder      = this.FileName.split( '.' )[ 0 ];
 				var file        = folder;
 				var contentType = { '3' : 'PD360', '6' : 'CC360' };
