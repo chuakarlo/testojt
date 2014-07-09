9751860fc15a6c96b0d3b0b93c0565955b02dfb3
Cameron Wengert <phantomxc@gmail.com>
2014-07-09 17:43:30 -0600

    Autolinker will strip out just numbers, make it a string ( 6346 )

--- js/apps/groups/views/GroupCommentView.js
+++ js/apps/groups/views/GroupCommentView.js
@@ -56,7 +56,8 @@ define( function ( require ) {
 		},
 
 		'onRender' : function () {
-			$( this.el ).find( 'p.wall-comment' ).prepend( autolinker.link( this.model.get( 'Message' ) ) );
+			var message = autolinker.link( _.escape( this.model.get( 'Message' ) ) );
+			$( this.el ).find( 'p.wall-comment' ).prepend( message );
 		},
 
 		'getTemplate' : function () {
