ec93b6332a42b7fb072179615349bddc159d94ae
Cameron Wengert <phantomxc@gmail.com>
2014-07-09 16:32:23 -0600

    Don't format URL again when navigating to UUV from search ( 6347 )

--- js/apps/search/views/SearchSegmentCardView.js
+++ js/apps/search/views/SearchSegmentCardView.js
@@ -5,7 +5,13 @@ define( function ( require ) {
 
 	return App.Common.SegmentCardsView.extend( {
 
-		'className' : 'col-xs-12 col-sm-6 col-md-3 col-lg-3'
+		'className' : 'col-xs-12 col-sm-6 col-md-3 col-lg-3',
+
+		'navigateToVideoPage' : function () {
+			// The URL is already properly formatted for UUV, no need to
+			// capture it in javascript
+			return;
+		}
 	} );
 
 } );
