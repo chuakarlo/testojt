a25bb5fa4df5212dff23d6610c8d56eb403e0ea7
Chad Dumadag <capdumadag@gmail.com>
2014-07-09 10:51:45 +0800

    Removed carousel snap

--- js/apps/homepage/external/widgets/views/UserWidgetCollectionView.js
+++ js/apps/homepage/external/widgets/views/UserWidgetCollectionView.js
@@ -1,50 +1,10 @@
 define( function ( require ) {
 	'use strict';
 
-	var Marionette           = require( 'marionette' );
-	var MAX_WIDGET_PEEK      = 70;
-	var MIN_WIDGET_PEEK      = 30;
-	var MAX_ALLOWABLE_MARGIN = 10;
+	var Marionette = require( 'marionette' );
 
 	var UserWidgetItemView      = require( 'apps/homepage/external/widgets/views/UserWidgetCompositeView' );
 	var EmptyUserWidgetItemView = require( 'apps/homepage/external/widgets/views/EmptyUserWidgetItemView' );
-
-	function addCarousel ( container, appendToID ) {
-		require( [ 'pc-carouselSnap' ], function ( $ ) {
-			$( container ).carouselSnap ( {
-				nextID        : 'next-slide-' + appendToID,
-				prevID        : 'previous-slide-' + appendToID,
-				startOnCenter : false,
-				rotate        : true,
-				beforeShift   : function () {},
-				afterShift    : function () {}
-			} );
-		} );
-	}
-
-/**
- * Adjust individual widget margin on different screen resolution if last Widget PEEK
- * percentage is less than 30%
- */
-
-	function adjustWidgetMargin ( container ) {
-		require( [ 'pc-adjustablePeek' ], function ( $ ) {
-			$( container ).adjustablePeek( {
-				'containerId'  : 'active-widgets',
-				'itemClass'    : 'widget-specific',
-				'minPeek'      : MIN_WIDGET_PEEK,
-				'maxPeek'      : MAX_WIDGET_PEEK,
-				'maxMargin'    : MAX_ALLOWABLE_MARGIN,
-				'beforeAdjust' : function () {
-					container.hide();
-				},
-				'afterAdjust'  : function () {
-					addCarousel( container, 'active-widgets' );
-					container.show();
-				}
-			} );
-		} );
-	}
 
 	return Marionette.CollectionView.extend( {
 		'tagName'   : 'ul',
@@ -60,7 +20,6 @@ define( function ( require ) {
 				var emptyUserWidgetItemView = new EmptyUserWidgetItemView();
 				self.$el.append( emptyUserWidgetItemView.render().el );
 			}
-			adjustWidgetMargin( this.$el );
 		}
 	} );
 } );
