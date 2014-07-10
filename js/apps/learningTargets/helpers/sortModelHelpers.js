9b2aa24c850122787237859fb84cce9f56b755fd
Carter Binarao <carter.binarao@globalzeal.net>
2014-07-10 10:25:48 +0800

    Refactored sorting of TaskName is Process of me
     Removed hidden xs in main.html in Learning Target

--- js/apps/learningTargets/helpers/sortModelHelpers.js
+++ js/apps/learningTargets/helpers/sortModelHelpers.js
@@ -6,21 +6,13 @@ define( function ( require ) {
 
 			model.Tasks.sort ( function ( a, b ) {
 
-				var order     = '*!@_.()#^&%-=+01234567989ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-				var aTaskName = a.TaskName.toString();
-				var bTaskName = b.TaskName.toString();
-				var nameA     = order.indexOf ( aTaskName[ 0 ] );
-				var nameB     = order.indexOf ( bTaskName[ 0 ] );
-
-				if ( nameA === nameB ) { // same first character, sort regular
-					if ( a < b ) {
-						return -1;
-					} else if ( a > b ) {
-						return 1;
-					}
-					return 0;
+				if ( a.TaskName < b.TaskName ) {
+					return -1;
+				}
+				if ( a.TaskName > b.TaskName ) {
+					return 1;
 				} else {
-					return nameA - nameB;
+					return 0;
 				}
 
 			} );
