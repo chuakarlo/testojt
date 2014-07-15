e829ff189fe147330f5a5d7a9c959770c1ac07ac
Michael Tuttle <openam@gmail.com>
2014-07-15 01:00:47 -0600

    Fix groups widget validation errors

--- js/apps/homepage/external/widgets/external/groupActivity/collections/WidgetCollection.js
+++ js/apps/homepage/external/widgets/external/groupActivity/collections/WidgetCollection.js
@@ -1,66 +1,30 @@
 define ( function ( require ) {
 	'use strict';
 
-	var Backbone         = require( 'backbone' );
-	var Remoting         = require( 'Remoting' );
-	var Session          = require( 'Session' );
-	var App              = require( 'App' );
-	var ValidationSchema = {
-		'title'      : 'Group Activity Validation Schema',
-		'type'       : 'object',
-		'required'   : [ 'Avatar', 'LicenseId', 'LicenseName' ],
-		'properties' : {
-			'Avatar'      : {
-				'type' : 'string'
-			},
-			'LicenseId'   : {
-				'type' : 'number'
-			},
-			'LicenseName' : {
-				'type' : 'string'
-			}
-		}
-	};
-	function widgetRequest ( personnelId ) {
-		return {
-			'path'   : 'com.schoolimprovement.pd360.dao.GroupService',
-			'method' : 'getValidGroupsByPersonnelIdOrderedByRecentActivity',
-			'args'   : {
-				'persId' : personnelId
-			}
-		};
-	}
+	var Backbone = require( 'backbone' );
+	var App      = require( 'App' );
 
-	var Collection = Backbone.Collection.extend( {
+	return Backbone.CFCollection.extend( {
+
+		'path' : 'ObservationService',
+
+		'getReadOptions' : function () {
+			return {
+				'path'   : 'com.schoolimprovement.pd360.dao.GroupService',
+				'method' : 'getValidGroupsByPersonnelIdOrderedByRecentActivity',
+				'args'   : {
+					'persId' : App.request( 'session:personnelId' )
+				}
+			};
+		},
+
+		// sort by most recent start date (as opposed to creation or completed date)
 		'comparator' : function ( model ) {
 			var date = model.get( 'LicenseContentTypeId' );
+
 			return -date;
 		}
+
 	} );
 
-	return Backbone.Collection.extend( {
-		'fetch' : function ( options ) {
-
-			var fetchingModels = Remoting.fetch( [ widgetRequest( Session.personnelId() ) ] );
-
-			App.when( fetchingModels ).done( function ( models ) {
-				App.Homepage.Utils.jsonVal( function ( err ) {
-					if ( !err ) {
-						options.success( new Collection( models[ 0 ] ) );
-						return;
-					}
-					App.vent.trigger( 'flash:message', {
-						'message' : 'Group Activity widget: JSon error'
-					} );
-				}, {
-					'schema' : ValidationSchema,
-					'data'   : models[ 0 ]
-				} );
-			} ).fail( function ( error ) {
-				App.vent.trigger( 'flash:message', {
-					'message' : App.Homepage.Utils.message.groupActivityErrMsg
-				} );
-			} );
-		}
-	} );
 } );
