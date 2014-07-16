db6c3af114751c33f480ee560c28eee1124fd0e6
Logan Brown <logan.brown@schoolimprovement.com>
2014-07-15 18:35:01 -0600

    Video resources open in new tab/window (6531)
    - Per PM decision, video resources no longer display a preview and instead open in a new tab or window

--- js/apps/videoPlayer/views/tabs/VideoResourceItemView.js
+++ js/apps/videoPlayer/views/tabs/VideoResourceItemView.js
@@ -3,14 +3,9 @@ define( function ( require ) {
 
 	var _           = require( 'underscore' );
 	var Marionette  = require( 'marionette' );
-	var $           = require( 'jquery' );
-	var App         = require( 'App' );
-	var PreviewView = require( 'videoPlayer/views/tabs/PreviewItemView' );
-	var NotifyView  = require( 'videoPlayer/views/tabs/NotifyItemView' );
 	var template    = require( 'text!videoPlayer/templates/tabs/videoResourceItemView.html' );
 	var utils       = require( 'videoPlayer/utils/utils' );
 
-	/*global ActiveXObject: false */
 	return Marionette.ItemView.extend( {
 
 		'template' : _.template( template ),
@@ -46,46 +41,7 @@ define( function ( require ) {
 		'previewFile' : function ( e ) {
 			e.preventDefault();
 
-			var previewPath = this.model.get( 'previewPath' );
-
-			if ( previewPath === '' ) {
-				return;
-			}
-
-			// disable preview popup in mobile devices
-			if ( this.clickEnable === true ) {
-				// check if ie has adobe reader installed
-				if ( $.browser.name === 'msie' && !this.checkForPdfPlugin() ) {
-					var notifyView = new NotifyView();
-					App.modalRegion.show( notifyView );
-				} else {
-					var pdfPreview = new PreviewView( { 'model' : this.model } );
-					App.modalRegion.show( pdfPreview, {
-						'className' : 'pdf-preview-modal'
-					} );
-				}
-			} else {
-				window.open( previewPath );
-			}
-		},
-
-		'checkForPdfPlugin' : function () {
-			var isAdobe = this.getPDFPlugin();
-			return ( ( isAdobe !== null ) && ( isAdobe !== undefined ) ) ? true : false;
-		},
-
-		// get pdf plugin in ie
-		'getPDFPlugin' : function () {
-			// load the activeX control
-			// AcroPDF.PDF is used by version 7 and later
-			// PDF.PdfCtrl is used by version 6 and earlier
-			return this.getActiveXObject( 'AcroPDF.PDF' ) || this.getActiveXObject( 'PDF.PdfCtrl' );
-		},
-
-		'getActiveXObject' : function ( name ) {
-			try {
-				return new ActiveXObject( name );
-			} catch ( e ) {}
+			window.open( this.model.get( 'downloadPath' ) );
 		}
 
 	} );
