d3057edf88bfdb7999b53f1241378aa3d28834f5
Logan Brown <logan.brown@schoolimprovement.com>
2014-07-15 23:17:48 -0600

    Fix for user profile avatar paths

--- js/common/helpers/getUserAvatarPath.js
+++ js/common/helpers/getUserAvatarPath.js
@@ -5,6 +5,9 @@ define( function ( require ) {
 
 	return function ( fileName ) {
 
+		// ugly hack because database added this path to all users
+		fileName = fileName.replace( 'PD360/uploads/avatars/profile/', '' );
+
 		fileName = fileName || getConfig( 'profileAvatarDefaultFileName' );
 
 		return getConfig( 'profileAvatarWebPath' ) + fileName;
