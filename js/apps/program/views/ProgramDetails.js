16440883787838675c5561563111ef3c7a194ecf
Nathanael O. Hinay <nathanael.hinay@globalzeal.net>
2014-07-22 11:41:22 +0800

    Added mobility support for program page (6624)
    - added Description button when viewing in mobile
    - description now hidden by default in mobile (xs-hidden)
    - description will now be shown on click of Description button
    - changed font size for Program title in mobile

--- js/apps/program/views/ProgramDetails.js
+++ js/apps/program/views/ProgramDetails.js
@@ -4,6 +4,7 @@ define( function ( require ) {
 
 	var _          = require( 'underscore' );
 	var Marionette = require( 'marionette' );
+	var $          = require( 'jquery' );
 
 	var template   = require( 'text!../templates/ProgramDetails.html' );
 
@@ -12,6 +13,14 @@ define( function ( require ) {
 		'tagName' : 'span',
 
 		'template' : _.template( template ),
+
+		'ui' : {
+			'descriptionButton' : '.description-button'
+		},
+
+		'events' : {
+			'click @ui.descriptionButton' : 'toggleMobileDescription'
+		},
 
 		'initialize' : function ( options ) {
 			_.bindAll( this );
@@ -30,6 +39,18 @@ define( function ( require ) {
 					return options.details.ProgramDescription;
 				}
 			};
+		},
+
+		'toggleMobileDescription' : function ( e ) {
+			e.preventDefault();
+
+			if ( $( '.description-indicator' ).hasClass( 'fa-chevron-right' ) ) {
+				$( '.description-indicator' ).removeClass( 'fa-chevron-right' ).addClass( 'fa-chevron-down' );
+			} else {
+				$( '.description-indicator' ).removeClass( 'fa-chevron-down' ).addClass( 'fa-chevron-right' );
+			}
+
+			$( '.description-text' ).toggleClass( 'hidden-xs' );
 		}
 
 	} );
