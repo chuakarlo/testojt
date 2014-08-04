4a65355dfd506cae1e6ef1ee3b32433260d8e320
Ronald R. Gemao <ronald.gemao@globalzeal.net>
2014-08-04 09:16:04 +0800

    Fixed defect #7108 - HOTFIX: iMobile- Searched Videos Do Not Play

--- js/apps/search/views/SearchSegmentCardView.js
+++ js/apps/search/views/SearchSegmentCardView.js
@@ -5,13 +5,7 @@ define( function ( require ) {
 
 	return App.Common.SegmentCardsView.extend( {
 
-		'className' : 'col-xs-12 col-sm-6 col-md-3 col-lg-3',
-
-		'navigateToVideoPage' : function () {
-			// The URL is already properly formatted for UUV, no need to
-			// capture it in javascript
-			return;
-		}
+		'className' : 'col-xs-12 col-sm-6 col-md-3 col-lg-3'
 
 	} );
 
